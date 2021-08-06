6cd0fb15402cd3cc8a79fbb40ff9a80651396dfd
timoshevichaleks <alti2692@gmail.com>
2021-08-06 15:21:11 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RequestsComponent } from './requests/requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestsComponent
   ],
   imports: [
     BrowserModule,
